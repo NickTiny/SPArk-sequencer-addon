f71c970ab1bfd8e562b2b55c3992cae26490fea1
Nick Alberelli <nick@tinymedia.ca>
2024-08-29 13:59:02 -0400

    Version Bump 0.1.1

--- spa_sequencer/__init__.py
+++ spa_sequencer/__init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author": "Nick Alberelli & The SPA Studios",
     "description": "Toolset to improve the sequence workflow in Blender.",
     "blender": (3, 3, 0),
-    "version": (0, 1, 0),
+    "version": (0, 1, 1),
     "location": "",
     "warning": "",
     "category": "SPA",
