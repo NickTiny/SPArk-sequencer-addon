a67ae215bb563e37b9b6b3c81ed7cc816ce6af91
Nick Alberelli <nick@tinymedia.ca>
2026-01-14 14:00:23 -0500

    Use Extensions Namespace for Imports
    https://docs.blender.org/manual/en/latest/advanced/extensions/addons.html#:~:text=Extensions%20and%20Namespace%C2%B6

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,9 @@
 # Copyright (C) 2023, The SPA Studios. All rights reserved.
 
 import bpy
-
+import importlib
+import addon_utils
+import sys
 
 def create_shot_scene(
     edit_scene: bpy.types.Scene, channel: int, frame_start: int
@@ -25,3 +27,13 @@ def create_shot_scene(
         frame_start=frame_start,
     )
     return shot_strip
+
+
+def get_module_name():
+    for module in addon_utils.modules():
+        if module.__name__.endswith("spa_sequencer"):
+            return module.__name__
+
+def import_spa_sequencer_module():
+    spa_sequencer = importlib.import_module(get_module_name())
+    sys.modules["spa_sequencer"] = spa_sequencer
