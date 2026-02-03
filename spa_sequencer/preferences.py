d39bf493d9c8adaaaa3be4fe072dd3257cfc9fcb
Nick Alberelli <nick@tinymedia.ca>
2026-02-02 19:57:04 -0500

    Add Preference to Enable/Disable Jump Keymap

--- spa_sequencer/preferences.py
+++ spa_sequencer/preferences.py
@@ -10,6 +10,16 @@ import bpy
 from .utils import register_classes, unregister_classes
 
 
+def update_jump_strip_keymap(self, context):
+    """Toggle jump strip keymaps when preference changes."""
+    from .keymaps import addon_keymaps
+    
+    # Toggle active state on existing keymap items
+    for km, kmi in addon_keymaps:
+        if kmi.idname == "sequence.strip_jump_anywhere":
+            kmi.active = self.enable_jump_strip_keymap
+
+
 class SPASequencerAddonPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
@@ -18,9 +28,24 @@ class SPASequencerAddonPreferences(bpy.types.AddonPreferences):
         description="Scene name prefix that identifies Shot Templates",
         default="TEMPLATE_SHOT",
     )
+    
+    enable_jump_strip_keymap: bpy.props.BoolProperty(
+        name="Enable Jump Strip Keymaps",
+        description=(
+            "Enable PageUp/PageDown keymaps for jumping between strips from anywhere. "
+            "When enabled, PageUp/PageDown will jump to previous/next strip in the master timeline"
+        ),
+        default=False,
+        update=update_jump_strip_keymap,
+    )
 
     def draw(self, context):
-        self.layout.prop(self, "shot_template_prefix")
+        layout = self.layout
+        layout.prop(self, "shot_template_prefix")
+        
+        box = layout.box()
+        box.label(text="Keymaps:", icon="KEYINGSET")
+        box.prop(self, "enable_jump_strip_keymap")
 
 
 def get_addon_prefs() -> SPASequencerAddonPreferences:
