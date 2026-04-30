6d7b431073ba6906212290c7e10f026b29bebc78
Nick Alberelli <nick@tinymedia.ca>
2026-04-30 10:51:01 -0400

    Move audition core logic to `shots/core.py`

--- spa_sequencer/shot/core.py
+++ spa_sequencer/shot/core.py
@@ -781,6 +781,41 @@ def make_meta_strip(strips: List[bpy.types.SceneStrip], name:str, frame_start:in
         strip.move_to_meta(meta_strip)
     return meta_strip
 
+def set_active_audition_strip(
+    audition_strip: bpy.types.MetaStrip, active_strip: bpy.types.SceneStrip
+):
+    """Set the name of the active audition strip and adjust timeline accordingly"""
+    for strip in audition_strip.strips:
+        if strip != active_strip:
+            strip.mute = True
+        else:
+            strip.mute = False
+
+    if active_strip.right_handle != audition_strip.right_handle:
+        offset = active_strip.right_handle - audition_strip.right_handle
+        adjust_shot_duration(audition_strip, offset)
+    audition_strip.audition.active = active_strip.name
+    audition_strip.name = f"Active: {active_strip.name}"
+
+
+def set_active_audition_strip(
+    audition_strip: bpy.types.MetaStrip, active_strip: bpy.types.SceneStrip
+):
+    """Set the name of the active audition strip and adjust timeline accordingly"""
+    for strip in audition_strip.strips:
+        if strip != active_strip:
+            strip.mute = True
+        else:
+            strip.mute = False
+
+    if active_strip.right_handle != audition_strip.right_handle:
+        offset = active_strip.right_handle - audition_strip.right_handle
+        adjust_shot_duration(audition_strip, offset)
+    audition_strip.audition.active = active_strip.name
+    audition_strip.name = f"Active: {active_strip.name}"
+
+
+
 class AuditionStripProperties(bpy.types.PropertyGroup):
     """Audition Strip Properties."""
 
